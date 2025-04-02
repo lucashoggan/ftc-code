423813d81475fa25a59441ac453eefa44dfc1f15
Lucas Hoggan <77905024+lucashoggan@users.noreply.github.com>
2025-04-02 18:40:09 +0100

    Fix CalcDistanceTPSRunningTime

--- Testing/AutoOpMode.java
+++ Testing/AutoOpMode.java
@@ -164,10 +164,10 @@ public class AutoOpMode extends OpMode {
 	public double[] CalcDistanceTPSRunningTime(double distance, double cms) {
 		double rollingForce = 0.0; // Force opposing forward rolling of the robot
 		double robotMass = 0.0;
-		double stoppingDistance = (robotMass*Math.pow(cms, 2))/(2*rollingForce);
+		double stoppingDistance = (robotMass*Math.pow((cms/100), 2))/(2*rollingForce);
 		
 		double output[] = new double[2];
-		double runningTime = (distance/cms) - (stoppingDistance/(cms/2));
+		double runningTime = (distance/cms) - ((stoppingDistance*100)/(cms/2));
 		double tpsSpeed =  VelocityCMStoTPS(cms);
 		output[0] = runningTime;
 		output[1] = tpsSpeed;
